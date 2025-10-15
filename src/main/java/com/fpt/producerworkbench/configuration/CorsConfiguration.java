478090e90ba4426591e0659bd1ee8f1df681c3f4
thanhvuqt48 <thanhvuqt48@gmail.com>
2025-10-15 16:49:28 +0700

    Complete CICD

--- src/main/java/com/fpt/producerworkbench/configuration/CorsConfiguration.java
+++ src/main/java/com/fpt/producerworkbench/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.fpt.producerworkbench.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@Slf4j
 public class CorsConfiguration {
 
-    @Value("${cors.allowed-origin}")
+    @Value("#{T(java.util.Arrays).asList('${cors.allowed-origins}'.split(','))}")
     private List<String> allowedOrigins;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        log.info("🌐 CORS Allowed Origins: {}", allowedOrigins);
         org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
         corsConfiguration.setAllowedOrigins(allowedOrigins);
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
