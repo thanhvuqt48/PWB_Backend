daafbe98d34a10ec62e0f230c96862e5a5b308f4
thanhvuqt48 <thanhvuqt48@gmail.com>
2025-12-16 02:04:41 +0700

    Add Complete Project Function & Milestone Status Logic

--- src/main/java/com/fpt/producerworkbench/service/ProjectService.java
+++ src/main/java/com/fpt/producerworkbench/service/ProjectService.java
@@ -2,7 +2,10 @@ package com.fpt.producerworkbench.service;
 
 import com.fpt.producerworkbench.dto.request.ProjectCreateRequest;
 import com.fpt.producerworkbench.entity.Project;
+import org.springframework.security.core.Authentication;
 
 public interface ProjectService {
     Project createProject(ProjectCreateRequest request, String creatorEmail);
+
+    Project completeProject(Long projectId, Authentication auth);
 }
